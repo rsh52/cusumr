e0d1497ebce86c14c7929018fb101593f6af9cec
Richard Hanna <hannar1@email.chop.edu>
2021-05-05 13:25:43 -0400

    Add testing and documentation

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(cusumr)
+
+test_check("cusumr")
---
+++ tests/testthat/test-cusumr.R
@@ -0,0 +1,6 @@
+test_that("cusumr works", {
+  df <- c(0,0,0,0,1,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1)
+
+  expect_type(cusumr(df), "list")
+  expect_true(sum(is.na(cusumr(df)$cusum_score)) == 0)
+})
